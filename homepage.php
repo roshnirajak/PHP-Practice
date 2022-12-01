43f4b17e9977f00d241382df33eed5604badd0f7
ROSH <imroshni3@gmail.com>
2022-12-01 18:34:49 +0530

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- <script src="https://code.jquery.com/jquery-3.6.1.js"
+        integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script> -->
+</head>
+
+<body>
+    <!-- <script>
+        $(function () {
+            $('#navbar').load('nav.php');
+        });
+    </script> -->
+    
+    <div id='navbar'>
+        <?php
+            include("nav.php");
+        ?>
+    </div>
+    <div class="mid">
+        <div class="mid-content"></div>
+    </div>
+</body>
+
+</html>
