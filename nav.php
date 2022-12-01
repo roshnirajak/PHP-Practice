e2ffd1caeb2c17b61e715eef97a58ea3bc35874b
ROSH <imroshni3@gmail.com>
2022-12-01 18:34:58 +0530

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul class="nav-ul">
+        <li><a href="homepage.php">home</a></li>
+        <li><a href="loginpage.php">login</a></li>
+        <li><a href="">registration</a></li>
+
+    </ul>
+</nav>
