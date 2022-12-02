c25e6b878bc95b346ac36bd760a18cae3c6b9c47
ROSH <imroshni3@gmail.com>
2022-12-03 00:02:48 +0530

    updated

--- loginpage.php
+++ loginpage.php
@@ -5,50 +5,136 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <link rel="icon" href="./assets/woo.jpg" type="image">
     <title>Login Page</title>
     <style>
+        @font-face {
+            font-family: 'calli';
+            src: url('./assets/calli.ttf');
+        }
+
+        @font-face {
+            font-family: 'bare';
+            src: url('./assets/BarefoodSignBrush-VGdaV.ttf');
+        }
+
         body {
             background: #fff;
             height: 100vh;
         }
-        nav{
-            border:none;
+
+        nav {
+            border: none;
         }
-        .nav-ul li a{
-            color:#538ea3;
+
+        .navbar {
+            margin-top: 3px;
         }
-        .nav-ul li a:hover{
+
+        .nav-ul {
+            display: flex;
+            justify-content: center;
+            list-style: none;
+        }
+
+        .nav-ul li {
+            padding: 10px;
+        }
+
+        .nav-ul li a {
+            color: #538ea3;
+            padding: 3px 10px 3px 10px;
+            text-decoration: none;
+            font-family: 'calli';
+            font-size: 22px;
+        }
+
+        .nav-ul li a:hover {
             background-color: #538ea318;
         }
+
+        .container {
+            margin-top: 8rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
         .container-in {
             height: 430px;
-            background-color:#10202a;
+            width: 340px;
+            text-align: center;
+            background-color: #10202a;
+            backdrop-filter: blur(5px);
+            box-shadow: rgba(0 0 0 / 89%) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+            border-radius: 25px;
         }
+
         .h1 {
+            height: 130px;
             background-image: url('./assets/bg2.jpg');
+            background-size: cover;
             background-position: bottom;
+            border-radius: 25px 25px 0 0;
+            padding-top: 25px;
+        }
+
+        .h1 a {
+            padding-top: 30px;
+            font-family: 'bare';
+            font-size: 50px;
+            color: #fff;
         }
 
         input {
-            margin-bottom: 4px;
-            font-size: 15px;
+            padding: 5px;
+            margin-bottom: 5px;
+            height: 23px;
+            width: 250px;
+            color: white;
+            font-size: 14px;
+            border: none;
+            background: rgba(255, 255, 255, 0.236);
         }
 
         ::placeholder {
             color: rgb(187, 187, 187);
         }
 
-        .register,
+        button {
+            margin-top: 5px;
+            margin-bottom: 10px;
+            padding: 5px;
+            height: 35px;
+            width: 260px;
+            border: none;
+            font-size: 15px;
+        }
+
+        .register{
+            margin-top: 8px;
+        }
         .register a {
-            font-size: 16px;
+            font-size: 17px;
             color: #fff;
+            font-family: 'Calibri';
         }
 
         #error {
             color: red;
             font-weight: 600;
+        }
+
+        @media screen and (max-width:440px) {
+            nav {
+                margin-left: 1%;
+                margin-right: 1%;
+            }
+
+            .container {
+                background-color: transparent;
+                backdrop-filter: blur(0px);
+            }
         }
     </style>
 </head>
@@ -70,7 +156,7 @@
                 <input type="password" name="pwd" id="pwd" placeholder="Password"><br />
                 <span id="error"></span><br />
                 <button type="submit">Submit</button><br>
-                <span class="register">-OR-<br><a href="registerpage.php">Register</a></span>
+                <div class="register"><a>-OR-</a><br><a href="registerpage.php">Register</a></div>
             </form>
         </div>
     </div>
--- registerpage.php
+++ registerpage.php
@@ -5,20 +5,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <link rel="icon" href="./assets/woo.jpg" type="image">
     <title>Registration Page</title>
     <style>
+        @font-face {
+            font-family: 'calli';
+            src: url('./assets/calli.ttf');
+        }
+
+        @font-face {
+            font-family: 'bare';
+            src: url('./assets/BarefoodSignBrush-VGdaV.ttf');
+        }
+
         body {
             background: #fff;
-            height:100vh
+            height: 100vh
         }
 
         nav {
             border: none;
         }
+
+        .nav-ul {
+            display: flex;
+            justify-content: center;
+            list-style: none;
+        }
+
+        .nav-ul li {
+            padding: 10px;
+        }
+
         .nav-ul li a {
             color: #cb202eba;
+            padding: 3px 10px 3px 10px;
+            text-decoration: none;
+            font-family: 'calli';
+            font-size: 22px;
         }
 
         .nav-ul li a:hover {
@@ -26,10 +50,14 @@
         }
 
         input {
+            padding: 5px;
+            height: 23px;
+            width: 250px;
             margin-bottom: 4px;
+            border: none;
             color: #fff;
             background: #ededed1f;
-            font-size: 14px;
+            /* font-size: 14px; */
         }
 
         ::placeholder {
@@ -37,22 +65,85 @@
         }
 
         button[type=submit] {
-            background-color:#dd2932db;
-            color:#fff;
-        }
-
-        .h1 {
-            background-image: url('./assets/bg33.png');
+            background-color: #dd2932db;
+            color: #fff;
         }
 
         form {
             margin-top: 20px;
         }
 
+        .container {
+            margin-top: 6.5rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .container-in {
+            height: 530px;
+            width: 340px;
+            text-align: center;
+            background-color: #000;
+            backdrop-filter: blur(5px);
+            box-shadow: rgba(0 0 0 / 89%) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+            border-radius: 25px;
+        }
+
+        .h1 {
+            height: 130px;
+            background-image: url('./assets/bg33.png');
+            background-size: cover;
+            background-position: center 30%;
+            border-radius: 25px 25px 0 0;
+            padding-top: 25px;
+        }
+
+        .h1 a {
+            padding-top: 30px;
+            font-family: 'bare';
+            font-size: 50px;
+            color: #fff;
+        }
+
+        input {
+            padding: 5px;
+            margin-bottom: 5px;
+            height: 23px;
+            width: 250px;
+            color: white;
+            font-size: 14px;
+            border: none;
+            background: rgba(255, 255, 255, 0.236);
+        }
+
+        button,
+        input[type=reset] {
+            margin-top: 8px;
+            margin-bottom: 5px;
+            padding: 5px;
+            height: 35px;
+            width: 120px;
+            border: none;
+            font-size: 15px;
+        }
+
         #error {
             color: red;
             font-weight: 600;
             text-align: center;
+        }
+
+        @media screen and (max-width:440px) {
+            nav {
+                margin-left: 1%;
+                margin-right: 1%;
+            }
+
+            .container {
+                background-color: transparent;
+                backdrop-filter: blur(0px);
+            }
         }
     </style>
 </head>
@@ -78,9 +169,10 @@
                 <input type="password" name="pwd" id="pwd" placeholder="Password" value=""><br />
                 <input type="password" name="confirm_pwd" id="con_pwd" placeholder="Confirm Password" value=""><br />
                 <span id="error"></span><br />
-                
-                    <button type="submit" style="margin-right:15px; margin-left:0;">Submit</button><input type="reset" value="Reset">
-                
+
+                <button type="submit" style="margin-right:15px; margin-left:0;">Submit</button><input type="reset"
+                    value="Reset">
+
             </form>
         </div>
 
