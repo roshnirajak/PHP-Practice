d0de617fba7360645ef03a235b8a58c97f47b27d
ROSH <imroshni3@gmail.com>
2022-12-02 22:06:12 +0530

    updated

--- homepage.php
+++ homepage.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="./assets/woo.jpg" type="image">
     <!-- <script src="https://code.jquery.com/jquery-3.6.1.js"
         integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script> -->
 </head>
