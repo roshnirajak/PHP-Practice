f744ad300c09cb8ae8fad7bf5af35e7d9c6f4995
ROSH <imroshni3@gmail.com>
2022-12-03 16:36:53 +0530

    updated

--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
     <ul class="nav-ul">
         <li><a href="homepage.php">home</a></li>
         <li><a href="loginpage.php">login</a></li>
-        <li><a href="registerpage.php">registration</a></li>
+        <li><a href="index.php">registration</a></li>
 
     </ul>
 </nav>
